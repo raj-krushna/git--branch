72cc12febd1b90ab97db058d76978f03a93f01f1
krushna raj <iloninatarj@gmail.com>
2018-03-22 11:31:19 +0530

    while.py is add in the qa branch

---
+++ while.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+
+count = input('enter the value:')
+while (count<20):
+  print "never give up", count
+  #count = count+1
+  #if (count == 12):
+  # break 
+  count = count+1
+  if (count == 11):
+   # print "never give up", count
+    continue
+print "for the god sake please this is enough"
