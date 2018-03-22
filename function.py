ae304c207e68ff2b71b17a80ef986ba75c5829cb
krushna raj <iloninatarj@gmail.com>
2018-03-22 10:57:36 +0530

    function.py file is add in the test

---
+++ function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+
+def raj(var1):
+    print var1
+    return
+#to call a function
+raj("never give up")
+
+#to print and calling a function
+print raj("happy morning")
+
+#to store the function in a variable
+a=raj("avenger")
+print a
